0288ec9ea32847d95f6d5f44d97380653b678b3d
Sukirno <mblonyox@gmail.com>
2022-10-12 17:02:41 +0700

    fix: cron reschedule for utc timezone

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,10 @@ module.exports = {
     name   : "mofjs-wa-reminder",
     script : "deno task start",
     exec_mode: 'fork',
-    cron_restart: "30 7,17 * * 1-5",
+    cron_restart: "30 0,10 * * 1-5",
     instances: 1,
     watch: false,
     autorestart: false
   }]
 }
+
